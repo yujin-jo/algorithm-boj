9f86149cd2a7262ceee4dbd00819b5520f07e42d
yujin-jo <jogrammer312@gmail.com>
2021-02-26 18:18:35 +0900

    Add Boj_10757.java

---
+++ step8/Boj_10757.java
@@ -0,0 +1,14 @@
+package step8;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Boj_10757 {
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner sc = new Scanner(System.in);
+        BigInteger A = sc.nextBigInteger();
+        BigInteger B = sc.nextBigInteger();
+
+        System.out.println(A.add(B));
+    }
+}
